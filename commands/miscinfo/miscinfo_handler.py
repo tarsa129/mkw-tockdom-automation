0b19149af4a5578b2a13740ab345cc02001fd853
tarsa129 <sheehant1293@gmail.com>
2025-06-09 22:14:12 -0400

    Get misc-info arguments.

--- commands/miscinfo/miscinfo_handler.py
+++ commands/miscinfo/miscinfo_handler.py
@@ -1,9 +1,12 @@
 from tockdomio import tockdomread, tockdomwrite
-from trackpage import track_page
+from commands.miscinfo import track_page_miscinfo as track_page
 
 def add_ids_by_pagename(pagename, ids_dict: dict):
     tockdom_response = tockdomread.get_page_text_by_name(pagename)
+    page_text:str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
+    arguments = track_page.get_miscinfo_template(page_text)
+    print(arguments)
 
 
 def handle_command(action, file):
-    pass
+    add_ids_by_pagename("User:Tarsa129/Test", {})
---
+++ commands/miscinfo/track_page_miscinfo.py
@@ -0,0 +1,30 @@
+from trackpage.mediawikiparse import *
+import warnings
+
+def get_ordered_miscinfo_arguments():
+    arguments = {}
+    expected_params = ["name", "image", "image-id", "creator", "author", "designer", "version", "date of release",
+                       "editors used", "video", "scale", "wbz-id", "download", "download 1", "download 2"]
+    for expected_param in expected_params:
+        arguments[expected_param] = ""
+    return arguments
+
+def get_miscinfo_template(page_text):
+    if not isinstance(page_text, WikiText):
+        page_text = read_text(page_text)
+    templates: list[Template] = page_text.templates
+    miscinfo_template: Template = None
+    for template in templates:
+        if template.name.strip() == "Misc-Info":
+            miscinfo_template = template
+    if miscinfo_template is None:
+        return None
+
+    arguments = get_ordered_miscinfo_arguments()
+    for argument in miscinfo_template.arguments:
+        if argument.name not in arguments:
+            warnings.warn("{} is not a valid miscinfo argument".format(argument.name))
+            continue
+        arguments[argument.name] = argument.value.strip()
+
+    return arguments
