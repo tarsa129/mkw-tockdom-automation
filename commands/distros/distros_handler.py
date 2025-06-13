418ce6acd496e1a5bface8efaeecc328d1c87f80
tarsa129 <sheehant1293@gmail.com>
2025-06-13 00:11:52 -0400

    Use parsed file results when doing actions.

--- commands/distros/distros_handler.py
+++ commands/distros/distros_handler.py
@@ -1,5 +1,6 @@
 from tockdomio import tockdomread, tockdomwrite
 from commands.distros import track_page_distros as track_page
+from commands.distros import distro_file as distro_file
 import warnings
 from enum import Enum
 from collections import Counter
@@ -47,6 +48,9 @@ def combine_distros(curr_distros: dict, new_distros: dict, action):
 def read_and_update_page(tockdom_response, new_distros, action):
     if not (validate_distros):
         raise RuntimeError("List of distros to add has repeats")
+    print(tockdom_response)
+    if not "pageid" in tockdom_response:
+        raise RuntimeError("Page with name {} does not exist".format(tockdom_response["title"]))
     page_id = tockdom_response["pageid"]
     page_text:str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
     curr_distros = track_page.get_distros_from_page(page_text)
@@ -64,23 +68,23 @@ def read_and_update_page(tockdom_response, new_distros, action):
 
     return response.json()["edit"]["result"]
 
-def edit_distros_to_pagename(pagename, new_distros: dict, action):
+def edit_distros_to_pagename(pagename, distros: dict, action):
     tockdom_response = tockdomread.get_page_text_by_name(pagename)
-    read_and_update_page(tockdom_response, new_distros, action)
+    read_and_update_page(tockdom_response, distros, action)
 
-def edit_distros_to_pageid(pageid, new_distros: dict, action):
+def edit_distros_to_pageid(pageid, distros: dict, action):
     tockdom_response = tockdomread.get_page_text_by_id(pageid)
-    read_and_update_page(tockdom_response, new_distros, action)
+    read_and_update_page(tockdom_response, distros, action)
+
+def edit_distros_to_pagenames(distros_to_add: dict, action):
+    for pagename, distros in distros_to_add.items():
+        edit_distros_to_pagename(pagename, distros, action)
 
 def handle_command(action, file):
-    added_distros = {}
-    added_distros["Aurora"] = "[[Aurora]] (v2.1)"
-    added_distros["tarsa's epic track pack"] = "[[tarsa's epic track pack]] (v2.0)"
-    added_distros["Tarsa's epic track pack 2"] = "{{Distrib-ref|Tarsa's epic track pack 2|129129|tarsa-pack}}"
+    pagename_to_distros = distro_file.read_distro_file(file)
     if action == "add":
-        edit_distros_to_pagename("User:tarsa129/Test", added_distros, action = Action.ADD)
+        edit_distros_to_pagenames(pagename_to_distros, action = Action.ADD)
     elif action == "update":
-        edit_distros_to_pagename("User:tarsa129/Test", added_distros, action = Action.UPDATE)
+        edit_distros_to_pagenames(pagename_to_distros, action = Action.UPDATE)
     elif action == "delete":
-        edit_distros_to_pagename("User:tarsa129/Test", added_distros, action = Action.DELETE)
-
+        edit_distros_to_pagenames(pagename_to_distros, action = Action.DELETE)
