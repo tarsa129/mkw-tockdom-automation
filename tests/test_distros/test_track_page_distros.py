82901dcc0ff2ad22f5f1036ee7ffad73451e9677
tarsa129 <sheehant1293@gmail.com>
2025-06-13 01:28:55 -0400

    Complete unit test for parsing distribution name

--- tests/test_distros/test_track_page_distros.py
+++ tests/test_distros/test_track_page_distros.py
@@ -5,11 +5,27 @@ import commands.distros.track_page_distros as tpd
 
 class TestTrackPageHandler(unittest.TestCase):
     def test_read_distro_name_wiikilink(self):
-        distro_text = "[[Distro Name]]"
+        actual_distro_name = "Distro Name"
+        distro_text = f"[[{actual_distro_name}]]"
         distro_name = tpd.read_distro_name(distro_text)
-        self.assertEqual(distro_name, "Distro Name")
+        self.assertEqual(distro_name, actual_distro_name)
 
     def test_read_distro_name_wiikilink_plus(self):
-        distro_text = "[[Distro Name]] (v2.0)"
+        actual_distro_name = "Distro Name"
+        distro_text = f"[[{actual_distro_name}]] (v2.0)"
         distro_name = tpd.read_distro_name(distro_text)
-        self.assertEqual(distro_name, "Distro Name")
+        self.assertEqual(distro_name, actual_distro_name)
+
+    def test_read_distro_name_template(self):
+        actual_distro_name = "Distro Name"
+        distro_text = f"{{{{Distrib-ref|{actual_distro_name}|129129|tarsa-pack}}}}"
+        distro_name = tpd.read_distro_name(distro_text)
+        self.assertEqual(distro_name, actual_distro_name)
+
+    def test_read_distro_name_template_invalid(self):
+        actual_distro_name = "Distro Name"
+        distro_text = f"{{{{Distrib-ref|{actual_distro_name}|129129}}}}"
+        self.assertRaises(AssertionError, tpd.read_distro_name, distro_text)
+
+        distro_text = f"{{{{Distrib-Ref|{actual_distro_name}|129129|tarsa-pack}}}}"
+        self.assertRaises(AssertionError, tpd.read_distro_name, distro_text)
