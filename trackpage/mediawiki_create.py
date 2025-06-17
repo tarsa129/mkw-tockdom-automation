18d6e567487f02b5a57b5bcec5cdb02fa37221bb
tarsa129 <sheehant1293@gmail.com>
2025-06-17 00:06:01 -0400

    Add library functions for getting and creating mediawiki components.

---
+++ trackpage/mediawiki_create.py
@@ -0,0 +1,8 @@
+def create_template_from_args(arguments: dict, template_name):
+    template_text = "{{" + template_name+ "\n"
+    for argument, value in arguments.items():
+        if value is None:
+            continue
+        template_text += f"|{argument}= {value}\n"
+    template_text += "}}"
+    return template_text
