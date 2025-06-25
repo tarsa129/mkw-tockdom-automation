65e22dffd3e0fbaa2e67cb2d4e49a533aaa924b5
tarsa129 <sheehant1293@gmail.com>
2025-06-24 23:11:29 -0400

    Create functionality for category-based bulk edits.

--- commands/miscinfo/miscinfo_handler.py
+++ commands/miscinfo/miscinfo_handler.py
@@ -1,4 +1,4 @@
-from tockdomio import tockdomread, tockdomwrite
+from tockdomio import tockdomread, tockdomwrite, tockdomread_category
 from commands.miscinfo import track_page_miscinfo as track_page
 from . import distro_page_distroinfo as distro_page
 import warnings
@@ -22,18 +22,36 @@ def add_ids_by_pagename(pagename, new_arguments: dict, update_existing=False):
     response = tockdomwrite.edit_section(page_id, 0, template_text)
     print(response)
 
-def convert_to_template(pagename):
-    tockdom_response = tockdomread.get_page_text_by_name(pagename)
-    page_id = tockdom_response["pageid"]
-    page_text:str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
+def convert_to_template(page_id, page_text):
     distro_info = distro_page.get_distroinfo_table(page_text)
     distro_info_arguments = distro_page.get_distroinfo_arguments(distro_info)
     template_text = distro_page.create_distroinfo_template(distro_info_arguments)
     section_text = str(distro_page.edit_distroinfo_section(page_text, template_text))
-    response = tockdomwrite.edit_section(page_id, 0, section_text, "Convert to Distro-Info template (via API)")
     print(section_text)
+    response = tockdomwrite.edit_section(page_id, 0, section_text, "Convert to Distro-Info template (via API)")
     print(response.json())
 
+def convert_to_template_from_entry(page_entry):
+    page_id = page_entry["pageid"]
+    page_text:str = page_entry["revisions"][0]["slots"]["main"]["content"]
+    convert_to_template(page_id, page_text)
+
+def convert_to_template_from_pagename(page_name):
+    tockdom_response = tockdomread.get_page_text_by_name(page_name)
+    convert_to_template_from_entry(tockdom_response)
+
+def convert_to_template_bulk(category_name):
+    success_count = 0
+    for page_entry in tockdomread_category.get_page_entries_of_category(category_name):
+        try:
+            convert_to_template_from_entry(page_entry)
+            success_count += 1
+        except Exception as e:
+            break
+        if success_count >= 20:
+            break
+
 def handle_command(action, file):
-    convert_to_template("Mini Mushroom Pack HNS")
+    #convert_to_template("KartRider Music Pack")
+    convert_to_template_bulk("Distribution/Music")
     #add_ids_by_pagename("User:Tarsa129/Test", {"wbz-id": "129129", "image-id":"129130", "download 1": "invalid"})
---
+++ tockdomio/tockdomread_category.py
@@ -0,0 +1,57 @@
+import requests
+from constants import *
+
+from .tockdomread import get_page_text_by_pageids
+
+def do_tockdom_query(base_params):
+    headers = {
+        'User-Agent': TOCKDOM_API_KEY,
+    }
+
+    response = requests.get(TOCKDOM_API, headers=headers, params=base_params)
+    return response.json()
+
+def get_pageids_of_category(category_name):
+    base_params = {
+        "action": "query",
+        "list": "categorymembers",
+        "cmtitle" : f"Category:{category_name}",
+        "format": "json",
+        "cmlimit": CATEGORYLIST_BULKCOUNT,
+    }
+    response = do_tockdom_query(base_params)
+
+    last_continue = {}
+    while True:
+        params = base_params.copy()
+        params.update(last_continue)
+
+        response = do_tockdom_query(params)
+
+        for result in response['query']['categorymembers']:
+            yield result
+
+        if 'continue' not in response:
+            break
+
+        last_continue = response['continue']
+
+def get_pageid_batch(category_name):
+    page_ids = []
+
+    i = 0
+    for page_id in get_pageids_of_category(category_name):
+        page_ids.append(page_id)
+        i += 1
+        if i >= TRACKPAGE_BULKCOUNT:
+            page_ids_copy = page_ids.copy()
+            page_ids = []
+            i = 0
+            yield page_ids_copy
+    yield page_ids
+
+def get_page_entries_of_category(category_name):
+    for page_batch in get_pageid_batch(category_name):
+        page_ids = [entry['pageid'] for entry in page_batch]
+        for page_entry in get_page_text_by_pageids(page_ids):
+            yield page_entry
