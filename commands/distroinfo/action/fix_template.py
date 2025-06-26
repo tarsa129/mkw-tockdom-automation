d898f4d3e88479128c93027b3ffb561b7b72495a
tarsa129 <sheehant1293@gmail.com>
2025-06-26 00:16:35 -0400

    Get distribution template from page.

--- commands/distroinfo/action/fix_template.py
+++ commands/distroinfo/action/fix_template.py
@@ -1,7 +1,9 @@
 from common_utils.basic_action import BasicAction
+from commands.distroinfo.utils import distro_page_edit as page_edit
 
 def fix_template(page_id, page_name, page_text):
     print(page_id, page_name, page_text)
 
+    template_text = str(page_read.get_distroinfo_template(page_text))
 def get_action():
     return BasicAction(fix_template)
--- commands/distroinfo/utils/distro_page_read.py
+++ commands/distroinfo/utils/distro_page_read.py
@@ -3,3 +3,7 @@ from trackpage.wiiki_page_parse import get_first_section_from_page, read_table_s
 def get_distroinfo_table(page_text):
     first_section = get_first_section_from_page(page_text)
     return read_table_sidecaption(first_section)
+
+def get_distroinfo_template(page_text):
+    first_section = get_first_section_from_page(page_text)
+    return get_template_with_name(first_section, "Distribution-Info")
