9b2b514c0b02935ae96cd6a0df37e403e7806f0b
tarsa129 <sheehant1293@gmail.com>
2025-06-29 10:23:47 -0400

    Added constants for category-wide pulling.

--- constants.py
+++ constants.py
@@ -1,7 +1,11 @@
 #Used for tockdom wiki
-TOCKDOM_API = "https://mkdd.org/mediawiki-1.43.0/api.php"
-
-TRACKPAGE_BULKCOUNT = 50
-CATEGORYLIST_BULKCOUNT = 500
+TOCKDOM_API = "https://wiki.tockdom.com/w/api.php"
 
 TOCKDOM_API_KEY = ""
+WIKI_BOT_USERNAME = "Username@Botname"
+WIKI_BOT_PASSWORD = ""
+
+CATEGORYLIST_BULKCOUNT = 500
+TRACKPAGE_BULKCOUNT = 50
+
+UPDATE_COUNT = 20
