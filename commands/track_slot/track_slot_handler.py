62c91f24da76791c07e88a11561f62158f141def
tarsa129 <sheehant1293@gmail.com>
2025-07-01 21:22:02 -0400

    Make category name configurable.

--- commands/track_slot/track_slot_handler.py
+++ commands/track_slot/track_slot_handler.py
@@ -3,4 +3,5 @@ from commands.track_slot.action import convert_to_slot_template as convert_actio
 def handle_command(args):
     action = args["action"]
     if action == "convert":
-        convert_action.get_action().action_from_pagename("The Great Apple War")
+        category_name = args["category_name"]
+        convert_action.get_action().action_from_category(category_name)
