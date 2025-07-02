7a2eaed250a36bc3cc5cea6363be3a16ae641354
tarsa129 <sheehant1293@gmail.com>
2025-07-01 23:50:15 -0400

    Validate start and end wbz ids.

--- commands/miscinfo/action/get_ids_from_szslibrary.py
+++ commands/miscinfo/action/get_ids_from_szslibrary.py
@@ -1,5 +1,9 @@
 from tockdomio import szslibrary_read
+from commands.miscinfo.utils.wbz_id_process import *
+
 
 def get_wbz_ids(start_id, end_id, file_path):
     szslibrary_response = szslibrary_read.get_by_wbz_id(start_id)
     print(szslibrary_response)
+    if not validate_start_end_wbz_ids(start_id, end_id):
+        return False
