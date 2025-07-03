86a589effdb3fe9803d34fc9188c62ce0565e5ad
tarsa129 <sheehant1293@gmail.com>
2025-07-02 20:45:03 -0400

    Create new template text given new argument.

--- commands/miscinfo/action/edit_ids_on_page.py
+++ commands/miscinfo/action/edit_ids_on_page.py
@@ -6,16 +6,7 @@ import warnings
 
 def add_ids_by_pageid(page_id, page_text, new_arguments: dict, update_existing=False):
     arguments = track_page.get_miscinfo_template(page_text)
-
-    for argument, value in new_arguments.items():
-        if not update_existing and argument in arguments and arguments[argument]:
-            warnings.warn("{} already exists and update flag is false".format(argument))
-            continue
-        elif argument not in arguments:
-            warnings.warn("{} not a valid argument!".format(argument))
-            continue
-        arguments[argument] = value
-
+    track_page.patch_miscinfo_template(arguments, new_arguments, update_existing)
     template_text = track_page.create_miscinfo_template(arguments)
     response = tockdomwrite.edit_section(page_id, 0, template_text)
     print(response)
@@ -25,6 +16,8 @@ def add_ids_from_file(filepath):
     for wbz_entry in wbzs_to_add:
         page_id = wbz_entry["pageid"]
         arguments = {"wbz_id": wbz_entry["wbz_id"], "image_id": wbz_entry["image_id"]}
+        page_id = wbz_entry["page_id"]
+        arguments = {"wbz-id": wbz_entry["wbz_id"], "image-id": wbz_entry["image_id"]}
         tockdom_response = tockdomread.get_page_text_by_id(page_id)
         page_text: str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
         add_ids_by_pageid(page_id, page_text, arguments, update_existing=False)
--- commands/miscinfo/utils/track_page_miscinfo.py
+++ commands/miscinfo/utils/track_page_miscinfo.py
@@ -7,7 +7,8 @@ def get_ordered_miscinfo_arguments():
     expected_params = ["name", "image", "image-id", "creator", "author", "designer", "version", "date of release",
                        "editors used", "video", "scale", "wbz-id", "download", "download 1", "download 2"]
     for expected_param in expected_params:
-        arguments[expected_param] = ""
+        arguments[expected_param] = None
+
     return arguments
 
 def get_miscinfo_template(page_text):
@@ -23,6 +24,20 @@ def get_miscinfo_template(page_text):
         arguments[argument.name] = argument.value.strip()
 
     return arguments
+
+def patch_miscinfo_template(arguments, new_arguments, update_existing):
+    for new_arg, new_val in new_arguments.items():
+        if new_arg not in arguments:
+            warnings.warn("{} not a valid argument!".format(new_arg))
+            continue
+
+        would_replace_existing = (new_arg in arguments) and arguments[new_arg] and arguments[new_arg] != new_val
+        if update_existing and would_replace_existing:
+            warnings.warn("{} already exists and update flag is false".format(new_arg))
+            continue
+
+        if new_val:
+            arguments[new_arg] = new_val
 
 def create_miscinfo_template(arguments: dict):
     return create_template_from_args(arguments, "Misc-Info")
