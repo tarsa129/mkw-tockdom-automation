0707f589841624dd2732e3dbeda7e1ade1cbc0a7
tarsa129 <sheehant1293@gmail.com>
2025-07-04 23:11:49 -0400

    Read distro files as .csvs with one distribution per row + version column.

--- commands/distro_list/utils/distro_file_reader.py
+++ commands/distro_list/utils/distro_file_reader.py
@@ -1,42 +1,48 @@
-import os.path
-import csv
+import re
 import warnings
 from collections import defaultdict
 
 from commands.distro_list.utils import track_page_distros
+from mediawiki.mediawiki_read import read_text
+from common_utils.file_reader import read_csv_file
 
+def does_distro_need_version(distro_text):
+    parsed_text = read_text(distro_text)
+    if len(parsed_text.wikilinks) > 0:
+        return True
+    elif len(parsed_text.templates) > 0:
+        distro_template = parsed_text.templates[0]
+        assert(distro_template.name == "Distrib-ref")
+        return False
+    return True
 
-def parse_row(i, row):
-    if len(row) == 0:
-        warnings.warn("Line #{} is empty".format(i))
-        return None, None
-    elif len(row[0]) == 0:
-        warnings.warn("Line #{} contains an empty page name".format(i))
-        return None, None
-    elif len(row) == 1:
-        warnings.warn("Line #{} for pagename {} does not contain distro_list".format(i, row[0]))
-        return None, None
+def validate_distro_entry(update_entry, line_num):
+    page_name = update_entry["page_name"]
+    distro_text = update_entry["distro_text"]
+    version = update_entry["version"]
 
-    pagename = row[0]
-    distros = {}
+    if not page_name or not distro_text:
+        warnings.warn(f"entry on line {line_num} does NOT have a valid pagename or distro name")
+        return False
+    if does_distro_need_version(distro_text) and not version:
+        warnings.warn(f"entry on line {line_num} does NOT have a version")
+        return False
 
-    for distro_text in row[1:]:
-        distro_name = track_page_distros.read_distro_name(distro_text)
-        if distro_name is not None:
-            distros[distro_name] = distro_text
-    return pagename, distros
+    return True
 
 def read_distro_file(file):
-    if file is None or not os.path.exists(file):
-        raise RuntimeError("File {} does not exist.".format(file))
+    raw_file_info = read_csv_file(file)
 
     pages = defaultdict(lambda: dict())
+    for i, entry in enumerate(raw_file_info):
+        if not validate_distro_entry(entry, i+1):
+            continue
 
-    with open(file, newline='', encoding="utf8") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",", quotechar="\"")
-        for i, row in enumerate(reader):
-            pagename, distros = parse_row(i, row)
-            if pagename is None:
-                continue
-            pages[pagename] |= distros
+        distro_text = entry["distro_text"]
+        distro_name = track_page_distros.read_distro_name(distro_text)
+        version = entry["version"]
+        if version:
+            distro_text = f"{distro_text} ({version})"
+
+        pages[entry["page_name"]] |= {distro_name: distro_text}
     return pages
