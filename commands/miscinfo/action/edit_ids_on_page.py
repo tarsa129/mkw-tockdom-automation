70d7ad18003b9d85c9a623ba9a598d5d2bc47ffc
tarsa129 <sheehant1293@gmail.com>
2025-07-06 12:13:13 -0400

    Add via pagename or page id.

--- commands/miscinfo/action/edit_ids_on_page.py
+++ commands/miscinfo/action/edit_ids_on_page.py
@@ -3,7 +3,7 @@ from commands.miscinfo.utils import track_page_edit as track_edit
 from common_utils.file_reader import read_csv_file
 from tockdomio import tockdomread, tockdomwrite
 
-def add_ids_by_pageid(page_id, page_text, new_arguments: dict, update_wbz=False):
+def add_ids_to_page(page_id, page_text, new_arguments: dict, update_wbz=False):
     print(new_arguments)
     arguments = track_page.get_miscinfo_template(page_text)
     track_edit.patch_ids_to_miscinfo_template(arguments, new_arguments, update_wbz)
@@ -17,17 +17,24 @@ def add_ids_by_pageid(page_id, page_text, new_arguments: dict, update_wbz=False)
     was_successful = response.json()["edit"]["result"] == "Success"
     return was_successful
 
-def add_ids_from_file(filepath):
-    wbzs_to_add: list[dict] = read_csv_file(filepath)
-    for wbz_entry in wbzs_to_add:
-        page_id = wbz_entry["page_id"]
-        arguments = {"wbz-id": wbz_entry["wbz_id"], "image-id": wbz_entry["image_id"]}
-        tockdom_response = tockdomread.get_page_text_by_id(page_id)
-        page_text: str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
-        add_ids_by_pageid(page_id, page_text, arguments, update_wbz=False)
+def add_ids_by_pageid(page_id, new_arguments: dict, update_existing=False):
+    tockdom_response = tockdomread.get_page_text_by_id(page_id)
+    page_text: str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
+    return add_ids_to_page(page_id, page_text, new_arguments, update_wbz=False)
 
 def add_ids_by_pagename(pagename, new_arguments: dict, update_existing=False):
     tockdom_response = tockdomread.get_page_text_by_name(pagename)
     page_id = tockdom_response["pageid"]
     page_text:str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
-    return add_ids_by_pageid(page_id, page_text, new_arguments, update_existing)
+    return add_ids_to_page(page_id, page_text, new_arguments, update_existing)
+
+def add_ids_from_file(filepath):
+    wbzs_to_add: list[dict] = read_csv_file(filepath)
+    for wbz_entry in wbzs_to_add:
+        arguments = {"wbz-id": wbz_entry["wbz_id"], "image-id": wbz_entry["image_id"]}
+
+        page_id = wbz_entry["page_id"]
+        if page_id:
+            add_ids_by_pageid(page_id, arguments, False)
+        elif wbz_entry["track_name"]:
+            add_ids_by_pagename(wbz_entry["track_name"], arguments, False)
