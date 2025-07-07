bcafbbbea3c7b4049fddfd1e637cfff738bcd766
tarsa129 <sheehant1293@gmail.com>
2025-07-07 00:00:44 -0400

    Write functionality to determine the page name of a track given the base page name + set of authors.

---
+++ common_utils/track_disambiguation.py
@@ -0,0 +1,49 @@
+import re
+import warnings
+
+from mediawiki.mediawiki_parse import read_wikilink
+from mediawiki.mediawiki_read import read_text
+from tockdomio import tockdomread
+
+def is_disambiguation_page(tockdom_response):
+    if "categories" not in tockdom_response:
+        raise RuntimeError(f"No category or page ID for page {tockdom_response['title']} - likely not valid.")
+
+    categories = tockdom_response["categories"]
+    for category_entry in categories:
+        category_name = category_entry["title"]
+        if category_name.startswith("Category:Disambiguation"):
+            return True
+
+    return False
+
+def read_authors(name_text):
+    search_authors_re = re.search(" \(([^(]*)\)$", name_text)
+    if not search_authors_re:
+        return set()
+    author_text = search_authors_re.group(1)
+    return set(re.split(', | & ', author_text))
+
+def get_authors_from_item_entry(item_text, base_page_name):
+    wikilinks = read_text(item_text).wikilinks
+    if len(wikilinks) == 0:
+        warnings.warn(f"Track implementation {item_text} of {base_page_name} is invalid due to lack of links.")
+    title, text = read_wikilink(wikilinks[0])
+    if base_page_name not in title:
+        return None
+    return read_authors(text), title
+
+def get_page_from_name_authors(base_page_name, authors: set[str]):
+    tockdom_response = tockdomread.get_page_text_by_name(base_page_name)
+    if not is_disambiguation_page(tockdom_response):
+        return base_page_name
+
+    page_text:str = tockdom_response["revisions"][0]["slots"]["main"]["content"]
+    page_list = read_text(page_text).get_lists()[0]
+
+    for track_variant in page_list.fullitems:
+        track_authors, title = get_authors_from_item_entry(track_variant, base_page_name)
+        if track_authors == authors:
+            return title
+
+    return None
---
+++ tests/test_common_utils/test_track_disambiguation.py
@@ -0,0 +1,48 @@
+import unittest
+
+from common_utils import track_disambiguation as td
+
+class TestTrackDisambiguation(unittest.TestCase):
+    def test_read_authors_one(self):
+        page_name = "Ew Retro Track (notTarsa129)"
+        expected_set = {"notTarsa129"}
+        actual_set = td.read_authors(page_name)
+        self.assertSetEqual(expected_set, actual_set)
+
+    def test_read_authors_two(self):
+        page_name = "Ew Retro Track (notTarsa129 & randouser)"
+        expected_set = {"notTarsa129", "randouser"}
+        actual_set = td.read_authors(page_name)
+        self.assertSetEqual(expected_set, actual_set)
+
+    def test_read_authors_three(self):
+        page_name = "Ew Retro Track (notTarsa129, nottarsa & randouser)"
+        expected_set = {"notTarsa129", "nottarsa", "randouser"}
+        actual_set = td.read_authors(page_name)
+        self.assertSetEqual(expected_set, actual_set)
+
+    def test_read_authors_four(self):
+        page_name = "Ew Retro Track (notTarsa129, nottarsa, stillnottarsa & randouser)"
+        expected_set = {"notTarsa129", "nottarsa", "stillnottarsa", "randouser"}
+        actual_set = td.read_authors(page_name)
+        self.assertSetEqual(expected_set, actual_set)
+
+    def get_page_from_name_authors(self):
+        # Probably not good practice to not mock external API calls in unit tests.
+        # This is why the name has been altered to NOT run as part of the test suite
+        # You can reenable it by editing the method name to have "test_" in front.
+
+        #test not a disambiguation
+        toads_temple = td.get_page_from_name_authors("Toad's Temple", {"LucioWins"})
+        self.assertEqual("Toad's Temple", toads_temple)
+
+        #test simple retro
+        snes_mc1_zpl = td.get_page_from_name_authors("SNES Mario Circuit 1", {"ZPL"})
+        self.assertEqual("SNES Mario Circuit 1 (ZPL)", snes_mc1_zpl)
+
+        #test version that does not exist, given the author
+        wicked_woods_kh = td.get_page_from_name_authors("Wicked Woods", {"The Bad Kevin"})
+        self.assertIsNone(wicked_woods_kh)
+
+if __name__ == '__main__':
+    unittest.main()
