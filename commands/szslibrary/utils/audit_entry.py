71ac65d8fe73d190ba681ecd05423f4f533937eb
tarsa129 <sheehant1293@gmail.com>
2025-07-07 23:17:11 -0400

    Add helper functions for AuditSzsLibraryEntry.

--- commands/szslibrary/utils/audit_entry.py
+++ commands/szslibrary/utils/audit_entry.py
@@ -6,6 +6,26 @@ class AuditSzsLibraryEntry:
         self.suggested_value = ""
         self.track_name_version = track_name_version
 
+    def add_audit_info(self, faulty_column, curr_value, suggested_value):
+        self.faulty_column = faulty_column
+        self.curr_value = curr_value
+        self.suggested_value = suggested_value
+
+    def set_should_be_false(self, faulty_column):
+        self.faulty_column = faulty_column
+        self.curr_value = True
+        self.suggested_value = False
+
+    def set_should_be_true(self, faulty_column):
+        self.faulty_column = faulty_column
+        self.curr_value = False
+        self.suggested_value = True
+
+    def get_if_has_audit_info(self):
+        if self.faulty_column:
+            return self
+        return None
+
     @classmethod
     def create_no_pageid(cls, wbz_id, track_name_version):
         audit_entry = cls(wbz_id, track_name_version)
