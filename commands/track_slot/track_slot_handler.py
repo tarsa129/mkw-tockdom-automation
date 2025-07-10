71f8f5d1da846254a1d2ef5c16ffe82d9db748e7
tarsa129 <sheehant1293@gmail.com>
2025-07-09 23:06:58 -0400

    Allow for skip-until to be defined in file.

--- commands/track_slot/track_slot_handler.py
+++ commands/track_slot/track_slot_handler.py
@@ -4,4 +4,5 @@ def handle_command(args):
     action = args["action"]
     if action == "convert":
         category_name = args["category_name"]
-        convert_action.get_action().action_from_category(category_name)
+        skip_until = args["skip_until"] if "skip_until" in args else None
+        convert_action.get_action().action_from_category(category_name, skip_until=skip_until)
