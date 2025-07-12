69c6d62a14ddf50588fd7a155348cf23a5b6308c
tarsa129 <sheehant1293@gmail.com>
2025-07-12 14:44:27 -0400

    Catch warnings in unittests for cleaner output.

--- tests/test_commands/test_miscinfo/test_action/test_get_ids_from_szslibrary.py
+++ tests/test_commands/test_miscinfo/test_action/test_get_ids_from_szslibrary.py
@@ -1,4 +1,5 @@
 import unittest
+import warnings
 
 from commands.miscinfo.action import get_ids_from_szslibrary as gifs
 from commands.miscinfo.utils.wbz_id_process import WBZInfo
@@ -17,38 +18,42 @@ class TestGetIdsFromSzslibrary(unittest.TestCase):
 
     def test_remove_unnecessary_entries_remove_duplicate_update(self):
         self.wbz_entries.append(WBZInfo(1, 4, 1, "Track 1", "v1.1", None, "hash1"))
-        actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
-        self.assertEqual(len(actual_entries), 3)
-        wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
-        self.assertEqual(len(wbz_id_1_entries), 1)
-        self.assertEqual(wbz_id_1_entries[0].image_id, 1)
+        with warnings.catch_warnings(record=True) as w:
+            actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
+            self.assertEqual(len(actual_entries), 3)
+            wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
+            self.assertEqual(len(wbz_id_1_entries), 1)
+            self.assertEqual(wbz_id_1_entries[0].image_id, 1)
 
     def test_remove_unnecessary_entries_keep_image_update(self):
         self.wbz_entries.append(WBZInfo(1, 4, 1, "Track 1", "v1.1", None, "hash4"))
-        actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
-        self.assertEqual(len(actual_entries), 3)
-        wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
-        self.assertEqual(len(wbz_id_1_entries), 1)
-        self.assertEqual(wbz_id_1_entries[0].image_id, 4)
+        with warnings.catch_warnings(record=True) as w:
+            actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
+            self.assertEqual(len(actual_entries), 3)
+            wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
+            self.assertEqual(len(wbz_id_1_entries), 1)
+            self.assertEqual(wbz_id_1_entries[0].image_id, 4)
 
     def test_remove_unnecessary_entries_keep_latest_image(self):
         self.wbz_entries.append(WBZInfo(1, 4, 1, "Track 1", "v1.1-alt", None, "hash1"))
         self.wbz_entries.append(WBZInfo(1, 5, 1, "Track 1", "v1.2", None,"hash1"))
         self.wbz_entries.append(WBZInfo(1, 6, 1, "Track 1", "v1.3", None,"hash1"))
         self.wbz_entries.append(WBZInfo(1, 7, 1, "Track 1", "v1.4", None,"hash1"))
-        actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
-        self.assertEqual(len(actual_entries), 3)
-        wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
-        self.assertEqual(len(wbz_id_1_entries), 1)
-        self.assertEqual(wbz_id_1_entries[0].image_id, 1)
+        with warnings.catch_warnings(record=True) as w:
+            actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
+            self.assertEqual(len(actual_entries), 3)
+            wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
+            self.assertEqual(len(wbz_id_1_entries), 1)
+            self.assertEqual(wbz_id_1_entries[0].image_id, 1)
 
     def test_remove_update_no_image(self):
         self.wbz_entries.append(WBZInfo(1, 4, 1, "Track 1", "v1.1", None,None))
-        actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
-        self.assertEqual(len(actual_entries), 3)
-        wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
-        self.assertEqual(len(wbz_id_1_entries), 1)
-        self.assertEqual(wbz_id_1_entries[0].image_id, 1)
+        with warnings.catch_warnings(record=True) as w:
+            actual_entries = gifs.remove_unnecessary_entries(self.wbz_entries)
+            self.assertEqual(len(actual_entries), 3)
+            wbz_id_1_entries = [e for e in actual_entries if e.wbz_id == 1]
+            self.assertEqual(len(wbz_id_1_entries), 1)
+            self.assertEqual(wbz_id_1_entries[0].image_id, 1)
 
 
 if __name__ == '__main__':
