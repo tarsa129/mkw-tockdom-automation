4224b43113c3bf6daabc81bcfa872d2d6ac7fb6b
tarsa129 <sheehant1293@gmail.com>
2025-07-11 23:59:44 -0400

    Add track_version_extra to WBZInfo to determine alt versions.

--- commands/miscinfo/utils/wbz_id_process.py
+++ commands/miscinfo/utils/wbz_id_process.py
@@ -3,12 +3,13 @@ import warnings
 from common_utils.szslibrary_helpers import *
 
 class WBZInfo:
-    def __init__(self, page_id, track_name, track_version, wbz_id, image_id, image_hash):
+    def __init__(self, wbz_id, image_id, page_id, track_name, track_version, version_extra,  image_hash):
+        self.wbz_id = wbz_id
+        self.image_id = image_id
         self.page_id = page_id
         self.track_name = track_name
         self.track_version = track_version
-        self.wbz_id = wbz_id
-        self.image_id = image_id
+        self.track_version_extra = version_extra
         self.image_hash = image_hash
 
     def __repr__(self):
@@ -21,7 +22,8 @@ def get_wbz_info(wbz_id):
 
     page_id = track_info["track_wiki"]
     track_name = get_full_trackname(track_info)
-    track_version = get_full_versionname(track_info)
+    track_version = track_info["track_version"]
+    track_version_extra = track_info["track_version_extra"] if track_info["track_version_extra"] else None
     orig_wbz_id = track_info["track_family"]
     image_hash = get_imagehash_by_id(wbz_id)
 
@@ -32,4 +34,4 @@ def get_wbz_info(wbz_id):
         warnings.warn(f"wbz id {wbz_id} does NOT have an image, and is a new track. Writing 0 for image-id")
         wbz_id = 0
 
-    return WBZInfo(page_id, track_name, track_version, orig_wbz_id, wbz_id, image_hash)
+    return WBZInfo(orig_wbz_id, wbz_id, page_id, track_name, track_version, track_version_extra, image_hash)
