f5c7326003c657a172a7e6da9bd6553c3f80d712
tarsa129 <sheehant1293@gmail.com>
2025-07-18 21:29:13 -0400

    Bugfix: Template class should handle and convert "-" <-> "_".

--- common_utils/track_page_utils/template_utils/template_utils.py
+++ common_utils/track_page_utils/template_utils/template_utils.py
@@ -6,16 +6,23 @@ class TockdomTemplate:
 
     def to_text(self):
         original_dict = vars(self)
+        dict_to_write = original_dict.copy()
 
         for required_param in self.__class__.required_params:
-            if original_dict[required_param] is None:
-                original_dict[required_param] = ""
+            if dict_to_write[required_param] is None:
+                dict_to_write[required_param] = ""
 
-        return create_template_from_args(vars(self), self.__class__.template_name)
+        for existing_key in list(dict_to_write.keys()):
+            new_key = existing_key.replace("_", "-")
+            dict_to_write[new_key] = dict_to_write.pop(existing_key)
+
+        return create_template_from_args(dict_to_write, self.__class__.template_name)
 
     def merge_with_dict(self, params):
         existing_params = list(vars(self).keys())
         for name, value in params.items():
+            name = name.replace("-", "_")
+
             if name not in existing_params:
                 raise RuntimeError(f"Param name {name} is not valid.")
             setattr(self, name, value)
