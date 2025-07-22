4952f40c6326bcb9a2b3b4f564ec058e89469171
tarsa129 <sheehant1293@gmail.com>
2025-07-22 18:54:23 -0400

    Write unique updaters for manual deletion.

--- commands/miscinfo/utils/wbz_id_process.py
+++ commands/miscinfo/utils/wbz_id_process.py
@@ -5,7 +5,7 @@ from common_utils.szslibrary_helpers import get_track_info, get_full_versionname
 from common_utils.track_page_utils.wiiki_name_utils.track_disambiguation import get_page_from_name_authors
 
 class WBZInfo:
-    def __init__(self, wbz_id, image_id, page_id, track_name, track_version, version_extra,  image_hash):
+    def __init__(self, wbz_id, image_id, page_id, track_name, track_version, version_extra,  image_hash, unique_editors):
         self.wbz_id = wbz_id
         self.image_id = image_id
         self.page_id = page_id
@@ -13,6 +13,7 @@ class WBZInfo:
         self.track_version = track_version
         self.track_version_extra = version_extra
         self.image_hash = image_hash
+        self.unique_editors = unique_editors
 
     def __repr__(self):
         base_text = f"{self.wbz_id}/{self.image_id}: {self.track_name} {self.track_version}"
@@ -34,6 +35,8 @@ def get_wbz_info(wbz_id):
     orig_wbz_id = track_info["track_family"]
     image_hash = get_imagehash_by_id(wbz_id)
     track_authors = set(track_info["track_author"].split(","))
+    track_updaters = set(track_info["track_editor"].split(","))
+    unique_updaters = track_updaters - track_authors
 
     if not page_id:
         track_name = get_page_from_name_authors(track_name, get_mod_type(track_info), track_authors)
@@ -46,4 +49,4 @@ def get_wbz_info(wbz_id):
         warnings.warn(f"wbz id {wbz_id} does NOT have an image, and is a new track. Writing 0 for image-id")
         wbz_id = 0
 
-    return WBZInfo(orig_wbz_id, wbz_id, page_id, track_name, track_version, track_version_extra, image_hash)
+    return WBZInfo(orig_wbz_id, wbz_id, page_id, track_name, track_version, track_version_extra, image_hash, unique_updaters)
