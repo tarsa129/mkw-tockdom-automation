19449d802aaa2b1adba4c2b8013a83e7d4ff439d
tarsa129 <sheehant1293@gmail.com>
2025-08-29 14:07:49 -0400

    Catch warnings to remove output.

--- tests/test_common_utils/test_track_page_utils/test_template_utils/test_misc_info_utils.py
+++ tests/test_common_utils/test_track_page_utils/test_template_utils/test_misc_info_utils.py
@@ -1,13 +1,15 @@
 import unittest
+import warnings
 
 from common_utils.track_page_utils.template_utils import misc_info_utils as miu
 
 class TextTrackPageMiscinfo(unittest.TestCase):
     def test_get_miscinfo_template(self):
         page_text = "{{Misc-Info\n|name= {{PAGENAME}}\n|download = link}}"
-        actual_dict = miu.get_miscinfo_template(page_text)
-        expected_dict = miu.get_ordered_miscinfo_arguments() | {"name": "{{PAGENAME}}", "download": "link"}
-        self.assertDictEqual(actual_dict, expected_dict)
+        with warnings.catch_warnings(record=True) as w:
+            actual_dict = miu.get_miscinfo_template(page_text)
+            expected_dict = miu.get_ordered_miscinfo_arguments() | {"name": "{{PAGENAME}}", "download": "link"}
+            self.assertDictEqual(actual_dict, expected_dict)
 
 if __name__ == '__main__':
     unittest.main()
