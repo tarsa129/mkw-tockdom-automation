c6a034169ec10861aa8f93d84730a37f7c2b91fc
tarsa129 <sheehant1293@gmail.com>
2026-03-04 22:17:55 -0500

    Add full list of allowed arguments.

--- common_utils/track_page_utils/template_utils/misc_info_utils.py
+++ common_utils/track_page_utils/template_utils/misc_info_utils.py
@@ -5,8 +5,13 @@ from mediawiki.mediawiki_read import get_template_with_name
 
 def get_ordered_miscinfo_arguments():
     arguments = {}
-    expected_params = ["name", "image", "image-id", "creator", "author", "designer", "version", "date of release",
-                       "editors used", "video", "scale", "wbz-id", "download", "download 1", "download 2", "download 3"]
+    expected_params = ["name",
+                       "image","image-id", "image2", "image-id2", "image3", "image-id3", "image4", "image-id4", "image5", "image-id5",
+                       "item-tab", "item-tab2", "item-tab3", "item-tab4", "item-tab5",
+                       "creator", "author", "designer", "version", "date of release",
+                       "editors used", "video", "scale", "wbz-id",
+                       "download", "download2", "download3", "download4", "download5", "download6",
+                       "download7", "download8", "download9", "download10"]
     for expected_param in expected_params:
         arguments[expected_param] = None
 
@@ -29,3 +34,10 @@ def get_miscinfo_template(page_text):
         arguments[converted_arg_name] = argument.value.strip()
 
     return arguments
+
+def get_download_links(misc_info_args):
+    download_links = [misc_info_args["download"],
+                      misc_info_args["download2"], misc_info_args["download3"], misc_info_args["download4"],
+                      misc_info_args["download5"], misc_info_args["download6"], misc_info_args["download7"],
+                      misc_info_args["download8"], misc_info_args["download9"], misc_info_args["download10"]]
+    return list(filter(lambda x: x is not None and x.strip(), download_links))
