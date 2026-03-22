efd0c65aec5cfa1fb87e7e30d667cc61f0d5256a
tarsa129 <sheehant1293@gmail.com>
2026-03-22 19:21:56 -0400

    Add check to see if the current wbz-id should be written to the page.

--- commands/miscinfo/action/get_ids_from_szslibrary.py
+++ commands/miscinfo/action/get_ids_from_szslibrary.py
@@ -24,12 +24,34 @@ def get_familyid_information(track_info: SZSLibraryTrackInfo):
     miscinfo_version = get_latest_version(page_id)
     return page_id, miscinfo_version
 
+def valid_image_id(track_info: SZSLibraryTrackInfo, miscinfo_version, has_image, current_entries):
+    # Initial wbz-ids should always be added.
+    if track_info.track_family == track_info.id_first:
+        return True
+    # If there is no image, it is not a valid image-id.
+    if not has_image:
+        return False
+    #The version on the template is a valid write.
+    if miscinfo_version == track_info.get_full_versionname():
+        return True
+    #If the latest version does not have an image (or has not been processed) use the latest official version as a backup
+    #Check if the current version is official
+    if not track_info.is_official_version():
+        return False
+    #Check that nothing is currently staged for the track.
+    family_id = track_info.track_family
+    if family_id not in current_entries:
+        return True
+    #Check that you are not overriding the actual current version. 
+    currently_written_version = current_entries[family_id].get_full_versionname()
+    return miscinfo_version != currently_written_version
 
 def get_wbz_ids(start_id, end_id, file_path):
     if not validate_start_end_wbz_ids(start_id, end_id):
         return False
 
     miscinfo_versions = {} # family_id: miscinfo_version
+    family_id_to_track_info = {} #family_id: track_info
 
     for i in range(int(start_id), int(end_id) + 1):
         track_info: SZSLibraryTrackInfo = get_track_info(i)
@@ -37,7 +59,6 @@ def get_wbz_ids(start_id, end_id, file_path):
             warnings.warn(f"wbz_id {i} failed at getting track info. Skipping.")
             continue
 
-    write_wbz_file(file_path, wbz_info_entries)
         family_id = track_info.track_family
         if family_id not in miscinfo_versions:
             track_info.track_wiki, latest_version_on_page = get_familyid_information(track_info)
@@ -47,5 +68,8 @@ def get_wbz_ids(start_id, end_id, file_path):
         if not has_image:
             warnings.warn(f"wbz_id {i} does not have an image.")
 
+        miscinfo_version = miscinfo_versions[family_id]
+        if valid_image_id(track_info, miscinfo_version, has_image, family_id_to_track_info):
+            family_id_to_track_info[family_id] = track_info
 
     return wbz_info_entries
