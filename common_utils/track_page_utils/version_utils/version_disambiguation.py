8932229f11d38263ec9f4e276164e5c000c53793
tarsa129 <sheehant1293@gmail.com>
2026-05-02 00:24:41 -0400

    Add check for base version in version disambiguation.

--- common_utils/track_page_utils/version_utils/version_disambiguation.py
+++ common_utils/track_page_utils/version_utils/version_disambiguation.py
@@ -11,13 +11,18 @@ def get_wbz_id_from_miscinfo(family_id, misc_info_arguments):
         if track_info.get_full_versionname() == misc_info_arguments["version"]:
             return track_info
         elif track_info.get_full_versionname() == misc_info_arguments["version"].replace(" ", ""):
-            print(f"{family_id}:Matching with SZSLib version {track_info.get_full_versionname()} without spaces.")
+            print(f"{family_id}: Matching with SZSLib version {track_info.get_full_versionname()} without spaces.")
             return track_info
 
     print(f"{family_id}: Could not find exact SZSLib match for {misc_info_arguments['version']}, searching for date {misc_info_arguments['date of release']} instead.")
 
+    def match_on_baseversion_date(x: SZSLibraryTrackInfo):
+        version_filter = misc_info_arguments["version"].startswith(x.track_version)
+        date_filter = x.track_created == misc_info_arguments["date of release"]
+        return version_filter and date_filter
+
     #If version names are unsynced (see TGAW), then we can go by the date
-    matching_dates = list(filter(lambda x: x.track_created == misc_info_arguments["date of release"], track_infos))
+    matching_dates = list(filter(lambda x: match_on_baseversion_date(x), track_infos))
     if not matching_dates:
         return None
 
@@ -26,7 +31,7 @@ def get_wbz_id_from_miscinfo(family_id, misc_info_arguments):
         return matching_dates[0]
 
     #if multiple track info entries match the date, take the latest official version.
-    offcial_matching_dates = list(filter(lambda x: x.is_official_version(), matching_dates))
-    if offcial_matching_dates:
-        return offcial_matching_dates[-1]
-    return matching_dates[-1]
+    official_matching_dates = list(filter(lambda x: x.is_official_version(), matching_dates))
+    if official_matching_dates:
+        return official_matching_dates[-1]
+    return matching_dates[0]
